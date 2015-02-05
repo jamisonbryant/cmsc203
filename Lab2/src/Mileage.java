2e241b371a41d97774e3d70e07ba42c7cb96c71f
Jamison Bryant <robojamison@gmail.com>
2015-02-05 23:47:47 +0000

    Added documentation comments as dictated by Task #6

--- Lab2/src/Mileage.java
+++ Lab2/src/Mileage.java
@@ -1,11 +1,21 @@
+/**
+ * Gives the user information on the efficiency (measured in MPG)
+ * of the engine of their car, given a number of miles driven and
+ * the gallons of gas used to drive the distance.
+ * 
+ * Author: Jamison Bryant
+ * Date: 2/5/2015
+ */
+
 import java.util.Scanner;
 
 public class Mileage {
 	public static void main(String[] args) {
 		// Declare variables
-		double miles, gallons;
-		double mpg;
-		Scanner scanner = new Scanner(System.in);
+		double miles;								// Miles driven
+		double gallons;								// Gallons (of gas) used
+		double mpg;									// MPG
+		Scanner scanner = new Scanner(System.in);	// User input getter
 		
 		// Display program intro
 		System.out.println("This program calculates MPG (miles per gallon)");
@@ -20,6 +30,9 @@ public class Mileage {
 		gallons = scanner.nextDouble();
 		
 		// Calculate MPG
+		// The purpose of this calculation is to determine the efficiency
+		// of the user's engine. The higher the MPG, the more efficient
+		// the engine (because less gas is used to go further).
 		mpg = miles / gallons;
 		
 		// Print results
