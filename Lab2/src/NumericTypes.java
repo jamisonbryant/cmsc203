e4b761fe0e8c7cd3470da36b14ad931915aa30af
Jamison Bryant <robojamison@gmail.com>
2015-02-05 23:39:34 +0000

    Fixed logical errors and implemented tasks

--- Lab2/src/NumericTypes.java
+++ Lab2/src/NumericTypes.java
@@ -1,18 +1,16 @@
 /**
-   This program demonstrates how numeric types and operators behave in Java
-*/
+ * This program demonstrates how numeric types and operators behave in Java
+ */
 
-//TASK #2  Add import statement here to use the Scanner class
-//TASK #2  (Alternate) Add import statment to use JOptionPane class
+import java.util.Scanner;
 
-public class NumericTypes
-{
-	public static void main (String [] args)
-	{
-		//TASK #2 Create a Scanner object here (not used for alternate)
+public class NumericTypes {
+	public static void main (String [] args) {
+		// Create a scanner object
+		Scanner scanner = new Scanner(System.in);
 	
-		//identifier declarations
-		final int NUMBER = 2 ;		// number of scores
+		// Identifiers
+		final int NUMBER = 2;		// number of scores
 		final int SCORE1 = 100;		// first test score
 		final int SCORE2 = 95;		// second test score
 		final int BOILING_IN_F = 212;  // freezing temperature
@@ -23,42 +21,46 @@ public class NumericTypes
 		//TASK #3 declare variables used here
 		//TASK #4 declare variables used here
 
-		// Find an arithmetic average
-		average = SCORE1 + SCORE2 / NUMBER;
-		output = SCORE1 + " and " + SCORE2 + " have an average of "
-				+ average;
+		// Find the average of two numbers
+		average = (int) (SCORE1 + SCORE2) / (double) NUMBER;
+		output = SCORE1 + " and " + SCORE2 + " have an average of " + average;
 		System.out.println(output);
 
-		// Convert Fahrenheit temperatures to Celsius
-		fToC = 5/9 * (BOILING_IN_F - 32);
-		output = BOILING_IN_F + " in Fahrenheit is " + fToC
-			     + " in Celsius.";
+		// Convert Fahrenheit to Celsius
+		fToC = (int) (5.0/9.0 * (BOILING_IN_F - 32));
+		output = BOILING_IN_F + " in Fahrenheit is " + fToC + " in Celsius.";
 		System.out.println(output);
-		System.out.println();		// to leave a blank line
+		System.out.println();
 
-		// ADD LINES FOR TASK #2 HERE
-		// prompt the user for first name
-		// read the user's first name
-		// prompt the user for last name
-		// read the user's last name
-		// concatenate the user's first and last names
-		// print out the user's full name 
+		// Prompt for and print the user's name
+		String firstName, lastName, fullName = "";
+		System.out.println("What is your first name? ");
+		firstName = scanner.nextLine();
+		System.out.println("What is your last name? ");
+		lastName = scanner.nextLine();
+		fullName = firstName + " " + lastName;
+		System.out.println(fullName);
 
-		System.out.println();		// to leave a blank line
+		System.out.println();
 		
-		// ADD LINES FOR TASK #3 HERE
+		// Modify the user's name
+		char firstInitial = firstName.charAt(0);
+		System.out.println("Your first initial: " + firstInitial);
+		fullName = fullName.toUpperCase();
+		System.out.println(fullName + " (your name) has " + fullName.length() + " characters");
 		// get the first character from the user's first name
 		// print out the user's first initial
 		// convert the user's full name to all capital letters
 		// print out the user's full name in all capital letters
 
-		System.out.println();		// to leave a blank line
+		System.out.println();
 		
-		// ADD LINES FOR TASK #4 HERE
-		// prompt the user for a diameter of a sphere
-		// read the diameter
-		// calculate the radius
-		// calculate the volume 
-		// print out the volume
+		// Calculate the volume of a sphere
+		double diameter, radius, volume;
+		System.out.println("Enter a diameter (double): ");
+		diameter = scanner.nextDouble();
+		radius = diameter / 2.0;
+		volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+		System.out.println("Volume of that sphere: " + volume);
 	}	
 }
