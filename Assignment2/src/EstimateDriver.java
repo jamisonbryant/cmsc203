ebe1c68126877de8095a5873fb19e659b7d9050c
Jamison Bryant <robojamison@gmail.com>
2015-02-10 21:07:32 +0000

    Added prompt and validation for square footage

--- Assignment2/src/EstimateDriver.java
+++ Assignment2/src/EstimateDriver.java
@@ -5,22 +5,49 @@ public class EstimateDriver {
 	private static final String title = "Handy Estimate Calculator";
 	
 	public static void main(String[] args) {
-		boolean valid = false;
+		boolean valid;
+		String input;
 		String choice;
+		int footage;
 		
+		// Prompt user for task choice
+		valid = false;
 		do {
-			// Prompt user for task choice
-			choice = JOptionPane.showInputDialog(null, 
+			input = JOptionPane.showInputDialog(null, 
 				"Enter a task to calculate an estimate for:\n(either " +
 				"\"painting\" or \"carpeting\")\n", title, 
 				JOptionPane.QUESTION_MESSAGE);
 			
 			// Validate user input
-			if(choice.equals("painting") || choice.equals("carpeting")) {
+			if(input.equals("painting") || input.equals("carpeting")) {
+				choice = input;
 				valid = true;
 			} else {
-				JOptionPane.showMessageDialog(null, "\"" + choice + "\" is " +
-						"not a valid task!", title, JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "\"" + input + "\" is " +
+					"not a valid task!", title, JOptionPane.ERROR_MESSAGE);
+			}
+		} while(!valid);
+		
+		// Prompt user for square footage
+		valid = false;
+		do {
+			// Prompt user for task choice
+			input = JOptionPane.showInputDialog(null, 
+				"Enter the square footage that will be painted/carpeted: ", 
+				title, JOptionPane.QUESTION_MESSAGE);
+			
+			// Validate user input
+			try {
+				footage = Integer.parseInt(input);
+				
+				if(footage <= 0) {
+					throw new NumberFormatException();
+				}
+				
+				valid = true;
+			} catch(NumberFormatException e) {
+				JOptionPane.showMessageDialog(null, "\"" + input + "\" is " +
+					"not a valid number!", title, JOptionPane.ERROR_MESSAGE);
 			}
 		} while(!valid);
 	}
