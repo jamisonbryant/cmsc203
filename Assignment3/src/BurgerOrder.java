a8ee8efa2c40500b7474a7ef2bfba0bf0875a546
Jamison Bryant <robojamison@gmail.com>
2015-03-03 01:04:29 +0000

    Completed Assignment 3

---
+++ Assignment3/src/BurgerOrder.java
@@ -0,0 +1,223 @@
+/**
+ * Keeps track of the items in a Hartatak Burger order and calculates the 
+ * order's subtotal, tax, and total.
+ *
+ * @author  Jamison Bryant (jbryan46@montgomerycollege.edu)
+ * @version 1.1
+ */
+public class BurgerOrder {
+	/**
+	 * The price of a hamburger (${@value}) 
+	 */
+	public static final double HAMBURGER_PRICE = 4.95;
+	
+	/**
+	 * The price of a cheeseburger (${@value}) 
+	 */
+	public static final double CHEESEBURGER_PRICE = 5.95;
+	
+	/**
+	 * The price of a double cheeseburger (${@value}) 
+	 */
+	public static final double DOUBLE_CHEESEBURGER_PRICE = 6.95;
+	
+	/**
+	 * The price of a condiment (${@value}) 
+	 */
+	public static final double CONDIMENT_PRICE = 0.25;
+	
+	/**
+	 * The sales tax ({@value} cents/dollar) 
+	 */
+	public static final double SALES_TAX = 0.0785;
+	
+	/**
+	 * The meal (hambuger, cheeseburger, or double cheeseburger)
+	 */
+	private String meal;
+	
+	/**
+	 * The number of condiments
+	 */
+	private int condiments;
+	
+	/**
+	 * The subtotal of the order
+	 */
+	private double subtotal;
+	
+	/**
+	 * The tax on the order
+	 */
+	private double tax;
+	
+	/**
+	 * The total of the order (subtotal plus tax)
+	 */
+	private double total;
+	
+	/**
+	 * Constructs a BurgerOrder object
+	 */
+	public BurgerOrder() {
+		meal = "hamburger";
+		condiments = 0;
+	}
+	
+	/**
+	 * Constructs a BurgerOrder object given a meal
+	 * 
+	 * @param meal The meal (see {@link BurgerOrder#meal})
+	 */
+	public BurgerOrder(String meal) {
+		this.meal = meal;
+		condiments = 0;
+	}
+	
+	/**
+	 * Constructs a BurgerOrder object given a meal and the number of
+	 * condiments
+	 * 
+	 * @param meal 		 The meal (see {@link BurgerOrder#meal})
+	 * @param condiments The number of condiments
+	 */
+	public BurgerOrder(String meal, int condiments) {
+		this.meal = meal;
+		this.condiments = condiments;
+	}
+
+	/**
+	 * Returns the order's meal
+	 * 
+	 * @return The meal
+	 */
+	public String getMeal() {
+		if(meal.equals("hamburger")) {
+			return "Hamburger";
+		} else if(meal.equals("cheeseburger")) {
+			return "Cheeseburger";
+		} else if(meal.equals("double_cheeseburger")) {
+			return "Double Cheeseburger";
+		} else {
+			throw new RuntimeException("Invalid meal type encountered: " + meal);
+		}
+	}
+	
+	/**
+	 * Returns the price of the order's meal
+	 * 
+	 * @return The price of the meal
+	 */
+	public double getMealPrice() {
+		if(meal.equals("hamburger")) {
+			return HAMBURGER_PRICE;
+		} else if(meal.equals("cheeseburger")) {
+			return CHEESEBURGER_PRICE;
+		} else if(meal.equals("double_cheeseburger")) {
+			return DOUBLE_CHEESEBURGER_PRICE;
+		} else {
+			throw new RuntimeException("Invalid meal type encountered: " + meal);
+		}
+	}
+
+	/**
+	 * Sets the meal
+	 * 
+	 * @param meal The meal (see {@link BurgerOrder#meal})
+	 */
+	public void setMeal(String meal) {
+		this.meal = meal;
+	}
+
+	/**
+	 * Gets the number of condiments
+	 * 
+	 * @return The number of condiments
+	 */
+	public int getCondiments() {
+		return condiments;
+	}
+	
+	/**
+	 * Returns the price of the order's condiments
+	 * 
+	 * @return The price of the meal
+	 */
+	public double getCondimentsPrice() {
+		return condiments * 0.25;
+	}
+
+	/**
+	 * Sets the number of condiments
+	 * 
+	 * @param condiments The number of condiments
+	 */
+	public void setCondiments(int condiments) {
+		this.condiments = condiments;
+	}
+	
+	/**
+	 * Adds one condiment
+	 */
+	public void addCondiment() {
+		condiments++;
+	}
+	
+	/**
+	 * Removes one condiment
+	 */
+	public void removeCondiment() {
+		condiments--;
+	}
+
+	/**
+	 * Gets the subtotal of the order
+	 * 
+	 * @return The subtotal of the order
+	 */
+	public double getSubtotal() {
+		// Reset subtotal
+		subtotal = 0;
+		
+		// Add meal price
+		if(meal.equals("hamburger")) {
+			subtotal += HAMBURGER_PRICE;
+		} else if(meal.equals("cheeseburger")) {
+			subtotal += CHEESEBURGER_PRICE;
+		} else if(meal.equals("double_cheeseburger")) {
+			subtotal += DOUBLE_CHEESEBURGER_PRICE;
+		}
+		
+		// Add condiments
+		subtotal += (condiments * 0.25);
+		
+		// Return result
+		return subtotal;
+	}
+
+	/**
+	 * Gets the tax on the order
+	 * 
+	 * @return The tax on the order
+	 */
+	public double getTax() {
+		// Calculate tax
+		tax = subtotal * SALES_TAX;
+		
+		// Return result
+		return tax;
+	}
+
+	/**
+	 * Gets the total of the order (see {@link BurgerOrder#total})
+	 * 
+	 * @return The total of the order
+	 */
+	public double getTotal() {
+		// Calculate total
+		total = subtotal + tax;
+		
+		// Return result
+		return total;
+	}
+}
---
+++ Assignment3/src/GuiDriver.java
@@ -0,0 +1,393 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+/**
+ * Creates the GUI and handles the user interaction of the application.
+ *
+ * @author  Jamison Bryant (jbryan46@montgomerycollege.edu)
+ * @version 1.2
+ */
+public class GuiDriver extends JPanel {
+	/**
+	 * The order being built.
+	 */
+	private BurgerOrder order;
+	
+	/**
+	 * A convenience variable for accessing the GUI subtotal text field.
+	 */
+	private JTextField subtotalTextField;
+	
+	/**
+	 * A convenience variable for accessing the GUI tax text field.
+	 */
+	private JTextField taxTextField;
+	
+	/**
+	 * A convenience variable for accessing the GUI total text field.
+	 */
+	private JTextField totalTextField;
+	
+	/**
+	 * Creates the GUI of the application.
+	 */
+	public GuiDriver() {
+		// Create GUI
+		super(new BorderLayout());
+		
+		// Create order
+		order = new BurgerOrder();
+		
+		// Create master panels
+		JPanel topMasterPanel = new JPanel();
+		JPanel bottomMasterPanel = new JPanel();
+		
+		// Create meal panel
+		JPanel mealPanel = new JPanel();
+		mealPanel.setLayout(new BoxLayout(mealPanel, BoxLayout.Y_AXIS));
+		mealPanel.setBorder(BorderFactory.createTitledBorder("Meal Type"));
+		
+			// Create meal radio buttons
+			MealRadioButtonListener listener = new MealRadioButtonListener(this);
+		
+			JRadioButton hamburgerButton = new JRadioButton("Hamburger");
+			hamburgerButton.setSelected(true);
+			hamburgerButton.setActionCommand("hamburger");
+			hamburgerButton.addActionListener(listener);
+			
+			JRadioButton cheeseburgerButton = new JRadioButton("Cheeseburger");
+			cheeseburgerButton.setActionCommand("cheeseburger");
+			cheeseburgerButton.addActionListener(listener);
+			
+			JRadioButton doubleCheeseburgerButton = new JRadioButton("Double Cheeseburger");
+			doubleCheeseburgerButton.setActionCommand("double_cheeseburger");
+			doubleCheeseburgerButton.addActionListener(listener);
+			
+			// Add meal radio buttons to button group
+			ButtonGroup mealTypeButtonGroup = new ButtonGroup();
+			mealTypeButtonGroup.add(hamburgerButton);
+			mealTypeButtonGroup.add(cheeseburgerButton);
+			mealTypeButtonGroup.add(doubleCheeseburgerButton);
+		
+		// Add meal radio buttons to panel
+		mealPanel.add(hamburgerButton);
+		mealPanel.add(cheeseburgerButton);
+		mealPanel.add(doubleCheeseburgerButton);
+			
+		// Create condiments panel
+		JPanel condimentsPanel = new JPanel();
+		condimentsPanel.setBorder(BorderFactory.createTitledBorder("Condiments"));
+		
+			// Create condiments subpanels
+			JPanel vegetableCondimentsPanel = new JPanel();
+			vegetableCondimentsPanel.setLayout(new BoxLayout(vegetableCondimentsPanel, BoxLayout.Y_AXIS));
+			JPanel sauceCondimentsPanel = new JPanel();
+			sauceCondimentsPanel.setLayout(new BoxLayout(sauceCondimentsPanel, BoxLayout.Y_AXIS));
+		
+			// Create condiments checkboxes
+			JCheckBox lettuceCheckbox = new JCheckBox("Lettuce");
+			lettuceCheckbox.setActionCommand("lettuce");
+			lettuceCheckbox.addActionListener(new CondimentsCheckboxListener(this, lettuceCheckbox));
+			
+			JCheckBox tomatoesCheckbox = new JCheckBox("Tomatoes");
+			tomatoesCheckbox.setActionCommand("tomatoes");
+			tomatoesCheckbox.addActionListener(new CondimentsCheckboxListener(this, tomatoesCheckbox));
+			
+			JCheckBox onionsCheckbox = new JCheckBox("Onions");
+			onionsCheckbox.setActionCommand("onions");
+			onionsCheckbox.addActionListener(new CondimentsCheckboxListener(this, onionsCheckbox));
+			
+			JCheckBox mayoCheckbox = new JCheckBox("Mayonnaise");
+			mayoCheckbox.setActionCommand("mayo");
+			mayoCheckbox.addActionListener(new CondimentsCheckboxListener(this, mayoCheckbox));
+			
+			JCheckBox ketchupCheckbox = new JCheckBox("Ketchup");
+			ketchupCheckbox.setActionCommand("ketchup");
+			ketchupCheckbox.addActionListener(new CondimentsCheckboxListener(this, ketchupCheckbox));
+			
+			JCheckBox mustardCheckbox = new JCheckBox("Mustard");
+			mustardCheckbox.setActionCommand("Mustard");
+			mustardCheckbox.addActionListener(new CondimentsCheckboxListener(this, mustardCheckbox));
+			
+			// Add condiments checkboxes to subpanels
+			vegetableCondimentsPanel.add(lettuceCheckbox);
+			vegetableCondimentsPanel.add(tomatoesCheckbox);
+			vegetableCondimentsPanel.add(onionsCheckbox);
+			
+			sauceCondimentsPanel.add(mayoCheckbox);
+			sauceCondimentsPanel.add(ketchupCheckbox);
+			sauceCondimentsPanel.add(mustardCheckbox);
+			
+		// Add condiments radio buttons to panel
+		condimentsPanel.add(vegetableCondimentsPanel);
+		condimentsPanel.add(sauceCondimentsPanel);	
+		
+		// Create action buttons panel
+		JPanel buttonsPanel = new JPanel();
+		
+			// Create action buttons
+			JButton getTotalButton = new JButton("Get Total");
+			getTotalButton.setActionCommand("get_total");
+			getTotalButton.addActionListener(new ActionButtonListener(this));
+			
+			JButton printRecieptButton = new JButton("Print Reciept");
+			printRecieptButton.setActionCommand("print_reciept");
+			printRecieptButton.addActionListener(new ActionButtonListener(this));
+			
+			JButton exitButton = new JButton("Exit");
+			exitButton.setActionCommand("exit");
+			exitButton.addActionListener(new ActionButtonListener(this));
+			
+			// Add action buttons to button group
+			ButtonGroup actionButtonButtonGroup = new ButtonGroup();
+			actionButtonButtonGroup.add(getTotalButton);
+			actionButtonButtonGroup.add(printRecieptButton);
+			actionButtonButtonGroup.add(exitButton);
+			
+		// Add action buttons to panel
+		buttonsPanel.add(getTotalButton, BorderLayout.PAGE_START);
+		buttonsPanel.add(printRecieptButton, BorderLayout.CENTER);
+		buttonsPanel.add(exitButton, BorderLayout.PAGE_END);
+		
+		// Create results panel
+		JPanel resultsPanel = new JPanel();
+		
+			// Create result types subpanels
+			JPanel subtotalResultsPanel = new JPanel();
+			JPanel taxResultsPanel = new JPanel();
+			JPanel totalResultsPanel = new JPanel();
+			
+			// Create results labels
+			JLabel subtotalLabel = new JLabel("Subtotal");
+			JLabel taxLabel = new JLabel("Tax");
+			JLabel totalLabel = new JLabel("Total");
+			
+			// Create results text fields
+			subtotalTextField = new JTextField("$0.00");
+			subtotalTextField.setEditable(false);
+			
+			taxTextField = new JTextField("$0.00");
+			taxTextField.setEditable(false);
+			
+			totalTextField = new JTextField("$0.00");
+			totalTextField.setEditable(false);
+			
+			// Add results controls to results subpanels
+			subtotalResultsPanel.add(subtotalLabel, BorderLayout.LINE_START);
+			subtotalResultsPanel.add(subtotalTextField, BorderLayout.CENTER);
+			
+			taxResultsPanel.add(taxLabel, BorderLayout.LINE_START);
+			taxResultsPanel.add(taxTextField, BorderLayout.CENTER);
+			
+			totalResultsPanel.add(totalLabel, BorderLayout.LINE_START);
+			totalResultsPanel.add(totalTextField, BorderLayout.CENTER);
+			
+		// Add results controls to results panels
+		resultsPanel.add(subtotalResultsPanel, BorderLayout.PAGE_START);
+		resultsPanel.add(taxResultsPanel, BorderLayout.CENTER);
+		resultsPanel.add(totalResultsPanel, BorderLayout.PAGE_END);
+		
+		// Add GUI controls to master panels
+		topMasterPanel.add(mealPanel, BorderLayout.LINE_START);
+		topMasterPanel.add(condimentsPanel, BorderLayout.LINE_END);
+		bottomMasterPanel.add(buttonsPanel, BorderLayout.LINE_START);
+		bottomMasterPanel.add(resultsPanel, BorderLayout.LINE_END);
+		
+		// Add GUI controls to GUI
+		add(topMasterPanel, BorderLayout.PAGE_START);
+		add(bottomMasterPanel, BorderLayout.PAGE_END);
+	}
+	
+	/**
+	 * Handles the action fired by clicking a meal radio button.
+	 *
+	 * @author Jamison Bryant (jbryan46@montgomerycollege.edu)
+	 */
+	public class MealRadioButtonListener implements ActionListener {
+		private GuiDriver driver;
+		
+		/**
+		 * Instantiates the action listener.
+		 * 
+		 * @param driver The GUI driver master class
+		 */
+		public MealRadioButtonListener(GuiDriver driver) {
+			this.driver = driver;
+		}
+		
+		/**
+		 * Does the action required when a radio button is clicked.
+		 * 
+		 * Passes the action command of the selected radio button to the GUI's
+		 * order, which uses it as an internal identifier for the meal type
+		 * and it's price.
+		 */
+		public void actionPerformed(ActionEvent e) {
+			String actionCommand = e.getActionCommand();
+			
+			if(actionCommand.equals("hamburger")) {
+				driver.order.setMeal("hamburger");
+			} else if(actionCommand.equals("cheeseburger")) {
+				driver.order.setMeal("cheeseburger");
+			} else if(actionCommand.equals("double_cheeseburger")) {
+				driver.order.setMeal("double_cheeseburger");
+			}
+		}
+	}
+	
+	/**
+	 * Handles the action fired by clicking a condiment checkbox.
+	 *
+	 * @author Jamison Bryant (jbryan46@montgomerycollege.edu)
+	 */
+	public class CondimentsCheckboxListener implements ActionListener {
+		private GuiDriver driver;
+		private JCheckBox checkbox;
+		
+		/**
+		 * Instantiates the action listener.
+		 * 
+		 * @param driver   The GUI driver master class
+		 * @param checkbox The checkbox being checked/unchecked
+		 */
+		public CondimentsCheckboxListener(GuiDriver driver, JCheckBox checkbox) {
+			this.driver = driver;
+			this.checkbox = checkbox;
+		}
+		
+		/**
+		 * Does the action required when a checkbox is clicked.
+		 * 
+		 * If the checkbox is selected, the order's internal condiments counter 
+		 * is incremented by 1. If the checkbox is not selected, the order's 
+		 * internal condiments counter is decremented by 1.
+		 */
+		public void actionPerformed(ActionEvent e) {
+			if(checkbox.isSelected()) {
+				driver.order.addCondiment();			
+			} else {
+				driver.order.removeCondiment();
+			}
+		}
+	}
+	
+	/**
+	 * Handles the action fired by clicking an action button.
+	 *
+	 * @author Jamison Bryant (jbryan46@montgomerycollege.edu)
+	 */
+	public class ActionButtonListener implements ActionListener {
+		private GuiDriver driver;
+		
+		/**
+		 * Instantiates the action listener.
+		 * 
+		 * @param driver The GUI driver master class
+		 */
+		public ActionButtonListener(GuiDriver driver) {
+			this.driver = driver;
+		}
+		
+		/**
+		 * Does the action required when an action button is pressed.
+		 * 
+		 * If the "Get Total" button is pressed (identified by the action
+		 * command "get_total") then the currently selected form controls are
+		 * passed to the GUI's order object and the subtotal is calculated,
+		 * then the GUI's text fields are updated.
+		 * 
+		 * If the "Print Reciept" button is pressed (identified by the action
+		 * command "print_reciept") then data about the order is fetched and
+		 * a newline-delimited string is built and displayed to the user in an
+		 * alert box.
+		 * 
+		 * Finally, if the "Exit" button is pressed (identified by the action
+		 * command "exit"), the application is terminated. Without benefits.
+		 */
+		public void actionPerformed(ActionEvent e) {
+			String actionCommand = e.getActionCommand();
+			
+			if(actionCommand.equals("get_total")) {
+				// Get order data
+				double subtotal = driver.order.getSubtotal();
+				double tax = driver.order.getTax();
+				double total = driver.order.getTotal();
+				
+				// Set up currency formatter
+				Currency usd = Currency.getInstance("USD");
+				NumberFormat money = NumberFormat.getCurrencyInstance(Locale.US);
+				money.setCurrency(usd);
+				
+				// Update text fields
+				driver.subtotalTextField.setText(money.format(subtotal));
+				driver.taxTextField.setText(money.format(tax));
+				driver.totalTextField.setText(money.format(total));
+			} else if(actionCommand.equals("print_reciept")) {
+				// Get order data
+				String meal = driver.order.getMeal();
+				double mealPrice = driver.order.getMealPrice();
+				int condiments = driver.order.getCondiments();
+				double condimentsPrice = driver.order.getCondimentsPrice();
+				double subtotal = driver.order.getSubtotal();
+				double tax = driver.order.getTax();
+				double total = driver.order.getTotal();
+				
+				// Set up currency formatter
+				Currency usd = Currency.getInstance("USD");
+				NumberFormat money = NumberFormat.getCurrencyInstance(Locale.US);
+				money.setCurrency(usd);
+				
+				// Build reciept string
+				String recieptString = "Welcome to Hartatak Burgers!\n\n";
+				recieptString += "Your order was:\n";
+				recieptString += " - 1 " + meal + ": " + money.format(mealPrice) + "\n";
+				recieptString += " - " + condiments + " condiments: " + money.format(condimentsPrice) + "\n\n";
+				recieptString += "Subtotal: " + money.format(subtotal) + "\n";
+				recieptString += "Tax: " + money.format(tax) + "\n";
+				recieptString += "Total: " + money.format(total) + "\n";
+				
+				// Display reciept dialog box
+				JOptionPane.showMessageDialog(this.driver, recieptString);
+			} else if(actionCommand.equals("exit")) {
+				System.exit(0);
+			}			
+		}
+	}
+	
+	/**
+	 * Launches the application.
+	 * 
+	 * @param args An array of command-line arguments
+	 */
+	public static void main(String[] args) {
+		// Create window
+		JFrame gui = new JFrame("Hartatak Burger App");
+		
+		// Configure window
+		gui.setSize(800, 600);
+		gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		gui.getContentPane().add(new GuiDriver());
+		
+		// Show window
+		gui.pack();
+		gui.setLocationRelativeTo(null);
+		gui.setVisible(true);
+	}
+}
+	
